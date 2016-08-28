bdac10220e5178733427248d03b8a2a08d2eb053
Aditi Mohanty <rheaditi@users.noreply.github.com>
2016-08-28 19:48:53 +0530

    placeholder

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
+var mailtester = {},
+	request = require('request'),
+	MAILTEST_IN_URL = 'http://api.mailtest.in/v1/';
 
-var exportable = {};
-exportable.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
-module.exports = exportable;
+function prevalidate (email){
+	// return  
+	// {
+	// 	valid: true,
+	// 	email: abc@xyz.abc,
+	// 	domain: xyz.abc
+	// };
+};
+
+
+
+mailtester.checkThese = function (){
+
+};
+
+module.exports = mailtester;
