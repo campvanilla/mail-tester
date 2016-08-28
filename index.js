fd04c6cd402baeb45cb0c37ea61e0137b1b8bedd
Aditi Mohanty <rheaditi@users.noreply.github.com>
2016-08-28 20:01:59 +0530

    partial: check()

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ function prevalidate (email){
 };
 
 
+mailtester.check = function (email, callback){
+	var validated = prevalidate(email), 
+		result = {},
+		requestURL = '';
+
+	if(validated.valid) {
+		requestURL = MAILTEST_IN_URL + validated.domain;
+		request(requestURL, function(error, response, body){
+			if(!error && response.statusCode === 200){
+				result = {
+					status: body.status,
+					code: body.code,
+					message: body.message,
+					email: email
+				};
+				callback(result);
+			}
+			else {
+				//something
+			}
+		});
+	}
+	else {
+		result = {
+			code: 0,
+			status: 'ERROR',
+			message: 'INVALID_EMAIL'
+		};
+		callback(result);
+	}
+};
 
 mailtester.checkThese = function (){
 
